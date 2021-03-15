07b230f1c495352d07a10ce38033bfe6d4b25031
gias2402 <gias2402@usherbrooke.ca>
2021-03-14 21:39:52 -0400

    Ajout de la classe ConvBatchNormReluBlock

--- src/models/CNNBlocks.py
+++ src/models/CNNBlocks.py
@@ -41,3 +41,15 @@ class ResidualBlock(nn.Module):
         output = F.relu(output)
         return output
 
+
+class ConvBatchNormReluBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=bias)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        output = self.relu(self.bn(self.conv(x)))
+        return output
