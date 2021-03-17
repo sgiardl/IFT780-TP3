d38112241ac2d3dbb4f30cde196f703c86c1cd59
ran2402 <nicolas.raymond2@usherbrooke.ca>
2021-03-17 11:49:08 -0400

    Commentaires changés

--- src/models/IFT725Net.py
+++ src/models/IFT725Net.py
@@ -45,19 +45,19 @@ class IFT725Net(CNNBaseModel):
         self.model = nn.Sequential(DenseBlock(3),                                 # 3 x 32 x 32 -> 99 x 32 x 32
                                    BaseBlock(99, 60, stride=2, kernel_size=4),    # 99 x 32 x 32 -> 60 x 15 x 15
                                    DenseBlock(60),                                # 60 x 15 x 15 -> 156 x 15 x 15
-                                   BaseBlock(156, 100, stride=2),                 # 80 x 15 x 15 ->  100 x 7 x 7
+                                   BaseBlock(156, 100, stride=2),                 # 156 x 15 x 15 ->  100 x 7 x 7
                                    BottleNeck(100, 25),                           # 100 x 7 x 7 -> 25 x 7 x 7
                                    DenseBlock(25),                                # 25 x 7 x 7 ->  121 x 7 x 7
                                    DenseBlock(121),                               # 121 x 7 x 7 -> 217 x 7 x 7
                                    DenseBlock(217),                               # 217 x 7 x 7 -> 313 x 7 x 7
-                                   BottleNeck(313, 250),                          # 293 x 7 x 7 -> 250 x 7 x 7
+                                   BottleNeck(313, 250),                          # 313 x 7 x 7 -> 250 x 7 x 7
                                    BottleNeck(250, 200),                          # 250 x 7 x 7 -> 200 x 7 x 7
                                    ResidualBlock(200, 150),                       # 200 x 7 x 7 -> 150 x 7 x 7
                                    ResidualBlock(150, 100),                       # 150 x 7 x 7 -> 100 x 7 x 7
                                    ResidualBlock(100, 80),                        # 100 x 7 x 7 -> 80 x 7 x 7
                                    BaseBlock(80, 60),                             # 80 x 7 x 7 -> 60 x 5 x 5
-                                   BaseBlock(60, 30, kernel_size=3),              # 60 x 5 x 5 -> 30 x 3 x 3
-                                   BaseBlock(30, 100, kernel_size=3),             # 30 x 3 x 3 -> 100 x 1 x 1
+                                   BaseBlock(60, 30),                             # 60 x 5 x 5 -> 30 x 3 x 3
+                                   BaseBlock(30, 100),                            # 30 x 3 x 3 -> 100 x 1 x 1
                                    nn.Flatten(),                                  # 100 x 1 x 1 -> 100
                                    nn.Linear(100, num_classes))                   # 100 -> num_classes
 
