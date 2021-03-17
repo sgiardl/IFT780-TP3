24a20536ef23ff16aeed4ed87059db7f27e95235
ran2402 <nicolas.raymond2@usherbrooke.ca>
2021-03-17 10:26:17 -0400

    Version finale du modèle

--- src/models/IFT725Net.py
+++ src/models/IFT725Net.py
@@ -42,25 +42,24 @@ class IFT725Net(CNNBaseModel):
         """
         super(IFT725Net, self).__init__(num_classes, init_weights)
 
-        self.model = nn.Sequential(BaseBlock(3, 10, stride=2, kernel_size=4),     # 3 x 32 x 32 -> 10 x 15 x 15
-                                   BaseBlock(10, 5, stride=2),                    # 10 x 15 x 15 ->  5 x 7 x 7
-                                   DenseBlock(5),                                 # 5 x 7 x 7 ->  101 x 7 x 7
-                                   ResidualBlock(101, 80),                        # 101 x 7 x 7 -> 80 x 7 x 7
-                                   BaseBlock(80, 80, padding=2),                  # 80 x 7 x 7 -> 80 x 9 x 9
-                                   ResidualBlock(80, 40),                         # 80 x 9 x 9 -> 40 x 9 x 9
-                                   BaseBlock(40, 40, padding=2),                  # 40 x 9 x 9 -> 40 x 11 x 11
-                                   ResidualBlock(40, 20),                         # 40 x 11 x 11 -> 20 x 11 x 11
-                                   DenseBlock(20, growth_rate=10),                # 20 x 11 x 11 -> 50 x 11 x 11
-                                   ResidualBlock(50, 25),                         # 50 x 11 x 11 -> 25 x 11 x 11
-                                   BottleNeck(25, 10),                            # 25 x 11 x 11 -> 10 x 11 x 11
-                                   BaseBlock(10, 10, stride=2),                   # 10 x 11 x 11 -> 10 x 5 x 5
-                                   DenseBlock(10, growth_rate=10),                # 10 x 5 x 5 -> 40 x 5 x 5
-                                   ResidualBlock(40, 20),
-                                   ResidualBlock(20, 10),
-                                   BaseBlock(10, 10),                             # 10 x 5 x 5 -> 10 x 3 x 3,
-                                   BaseBlock(10, 100),             # 10 x 4 x 4 -> 100 x 1 x 1
-                                   nn.Flatten(),
-                                   nn.Linear(100, num_classes))
+        self.model = nn.Sequential(DenseBlock(3),                                 # 3 x 32 x 32 -> 99 x 32 x 32
+                                   BaseBlock(99, 60, stride=2, kernel_size=4),    # 99 x 32 x 32 -> 60 x 15 x 15
+                                   DenseBlock(60),                                # 60 x 15 x 15 -> 156 x 15 x 15
+                                   BaseBlock(156, 100, stride=2),                 # 80 x 15 x 15 ->  100 x 7 x 7
+                                   BottleNeck(100, 25),                           # 100 x 7 x 7 -> 25 x 7 x 7
+                                   DenseBlock(25),                                # 25 x 7 x 7 ->  121 x 7 x 7
+                                   DenseBlock(121),                               # 121 x 7 x 7 -> 217 x 7 x 7
+                                   DenseBlock(217),                               # 217 x 7 x 7 -> 313 x 7 x 7
+                                   BottleNeck(313, 250),                          # 293 x 7 x 7 -> 250 x 7 x 7
+                                   BottleNeck(250, 200),                          # 250 x 7 x 7 -> 200 x 7 x 7
+                                   ResidualBlock(200, 150),                       # 200 x 7 x 7 -> 150 x 7 x 7
+                                   ResidualBlock(150, 100),                       # 150 x 7 x 7 -> 100 x 7 x 7
+                                   ResidualBlock(100, 80),                        # 100 x 7 x 7 -> 80 x 7 x 7
+                                   BaseBlock(80, 60),                             # 80 x 7 x 7 -> 60 x 5 x 5
+                                   BaseBlock(60, 30, kernel_size=3),              # 60 x 5 x 5 -> 30 x 3 x 3
+                                   BaseBlock(30, 100, kernel_size=3),             # 30 x 3 x 3 -> 100 x 1 x 1
+                                   nn.Flatten(),                                  # 100 x 1 x 1 -> 100
+                                   nn.Linear(100, num_classes))                   # 100 -> num_classes
 
     def forward(self, x):
         return self.model(x)
